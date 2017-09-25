16b2dae64f34e2c43f220a8f2c2c85e652d8a492
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-09-24 22:42:31 -0500

    refactor code

--- HarmlessRansomNote.js
+++ HarmlessRansomNote.js
@@ -1,23 +1,31 @@
+/* Given an arbitrary ransom note string and another string containing letters
+from all the magazines, write a function that will return true if the ransom
+note can be constructed from the magazines ; otherwise, it will return false. */
 
+harmlessRansomNote = (noteText, magazineText) => {
+  let noteArr = noteText.split(' '); //convert the string into an array
+  let magazineArr = magazineText.split(' ');
 
-function harmlessRansomNote(noteText, magazineText) {
-  var noteArr = noteText.split(' ');
-  var magazineArr = magazineText.split(' ');
-  var magazineObj = {};
+  let magazineObj = {};
 
   magazineArr.forEach(word => {
-    if (!magazineObj[word]) magazineObj[word] = 0;
-    magazineObj[word]++;
-  });
 
-  var noteIsPossible = true;
-  noteArr.forEach(word => {
-    if (magazineObj[word]) {
-      magazineObj[word]--;
-      if (magazineObj[word] < 0) noteIsPossible = false;
+    if (!magazineObj[word]) {
+      magazineObj[word] = 0;
     }
-    else noteIsPossible = false;
+    magazineObj[word]++; //increment existing word
   });
+  // console.log(magazineObj);
 
-  return noteIsPossible;
+  let noteExist = true;
+  noteArr.forEach(word => {
+    if(magazineObj[word]) {
+      magazineObj[word]--; //decrement existing word
+      if(magazineObj < 0) noteExist = false; //return false if it doesn't exist
+    }
+    else noteExist = false;
+  });
+  return noteExist;
 }
+
+// harmlessRansomNote('this is all','this is all the magazine text in the magazine'); //test
