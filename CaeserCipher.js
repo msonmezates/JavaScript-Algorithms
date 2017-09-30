df8b1171cf33e28c713569da9fe33c64eb4a8497
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-09-29 21:42:33 -0500

    implement caesar cipher algorithm

---
+++ CaeserCipher.js
@@ -0,0 +1,26 @@
+/*write a function that takes a string and number such that each letter of a
+given text is replaced by a letter some fixed number of positions down the alphabet */
+
+function caeserCipher(str, num) {
+  let lowerCaseString = str.toLowerCase();
+  let alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split(''); //alphabet array
+
+  let newString = '';
+  for(let i=0; i<lowerCaseString.length; i++) {
+    let currentLetter = lowerCaseString[i];
+    if(currentLetter === ' ') {
+      newString += currentLetter;
+      continue; //it means move onto the next iteration of the for loop
+    }
+    let currentIndex = alphabetArray.indexOf(currentLetter); //find the index of current letter
+    let newIndex = currentIndex + num; //this is the new index of our alphabet array
+
+    if(newIndex > 25) newIndex = newIndex - 26; //make sure to stay in index 0(a) and 25(z)
+    if(newIndex < 0) newIndex = 26 + newIndex;
+
+    newString += alphabetArray[newIndex]; //new string with shifted letters
+  }
+  console.log(newString);
+}
+
+caeserCipher('ab',2); //cd
