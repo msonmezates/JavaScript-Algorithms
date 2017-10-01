2b3baa52a2d2660c51b31cb1894e9d5c7462bb7e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-09-30 21:17:40 -0500

    finish Reverse Words algorithm

---
+++ ReverseWords.js
@@ -0,0 +1,18 @@
+//write a function that takes a string and reverses each word
+
+reverseWords = str => {
+  let strArr = str.split(' ');
+  let newArr = [];
+
+  strArr.map(word => {
+    let newWord = '';
+    for(let i=word.length-1; i >= 0; i--) {
+      let currentLetter = word[i];
+      newWord += currentLetter;
+    }
+    newArr.push(newWord);
+  });
+  return newArr.join(' ');
+}
+
+reverseWords('this is a JavaScript string');
