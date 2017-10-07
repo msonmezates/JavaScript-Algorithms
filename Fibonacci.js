524776da684b3ad6e0466065764bea9234ba5adb
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-07 13:23:38 -0500

    start fibonacci with recursive

---
+++ Fibonacci.js
@@ -0,0 +1,10 @@
+/* Write a function of Fibonnaci series that takes position as a number and
+return that number in the position */
+
+fibonacci = position => {
+
+}
+
+//Testing
+fibonacci(4); //return 3
+fibonacci(9); //returns 34
