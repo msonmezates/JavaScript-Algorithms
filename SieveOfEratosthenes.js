94a311db8bd877b35c9d2321ffe2cdd2342033b7
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-11 15:33:29 -0500

    correct solution

--- SieveOfEratosthenes.js
+++ SieveOfEratosthenes.js
@@ -2,16 +2,25 @@
 numbers up to number in an array */
 
 sieveOfEratosthenes = num => {
-  let arr = [];
-
-  for(let i=0; i<num; i++) {
-    arr.push(i);
+  let primes = [];
+  for(let i=0; i<=num; i++) {
+    primes[i] = true;         //set each array element to true;
   }
 
-  arr = arr.filter(num => {
-    return (num%2 !== 0 && num%3 !== 0)
-  });
-  return arr;
+  primes[0] = false;
+  primes[1] = false;
+
+  for(let i=2; i <= Math.sqrt(num); i++) { // outer loop provides optimization so that we don't                                          // have to loop through each number in the array
+    for(let j=2; j*i <= num; j++ ) {
+      primes[i*j] = false;
+    }
+  }
+
+  let result = [];
+  for(let i=0; i<primes.length; i++) {
+    if(primes[i]) result.push(i);
+  }
+  return result;
 }
 
 sieveOfEratosthenes(20); //returns [2,3,5,7,11,13,17,19]
