6522605e63dd788886a9572f79e091724b10725a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-11 12:36:49 -0500

    finish memoized fibonacci algorithm

--- MemoizedFibonacci.js
+++ MemoizedFibonacci.js
@@ -2,7 +2,16 @@
 and return the position of the index  */
 
 fibMemo = (index, cache) => {
+  cache = cache || [];
 
+  if(cache[index]) return cache[index];
+  else {
+    if(index < 3) return 1;
+    else cache[index] = fibMemo(index-1, cache) + fibMemo(index-2, cache);
+  }
+
+  return cache[index];
 }
 
-fibMemo(20);
+//testing
+fibMemo(15); //return 610
