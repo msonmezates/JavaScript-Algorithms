9c23a93f67b47bd83d1e35b6cb5f94d169946b2f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-11 14:43:08 -0500

    finish algorithm

--- SieveOfEratosthenes.js
+++ SieveOfEratosthenes.js
@@ -2,7 +2,16 @@
 numbers up to number in an array */
 
 sieveOfEratosthenes = num => {
+  let arr = [];
 
+  for(let i=0; i<num; i++) {
+    arr.push(i);
+  }
+
+  arr = arr.filter(num => {
+    return (num%2 !== 0 && num%3 !== 0)
+  });
+  return arr;
 }
 
 sieveOfEratosthenes(20); //returns [2,3,5,7,11,13,17,19]
