731c670c0e5ef08b923f8a53e8f91d1f091d97a9
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-11 14:28:34 -0500

    start Sieve of Eratosthenes algorithm

---
+++ SieveOfEratosthenes.js
@@ -0,0 +1,8 @@
+/* Write a function that takes number as an argument and return all prime
+numbers up to number in an array */
+
+sieveOfEratosthenes = num => {
+
+}
+
+sieveOfEratosthenes(20); //returns [2,3,5,7,11,13,17,19]
