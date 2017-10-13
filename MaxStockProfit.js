d42c410671cc8278f54d4e65e2845ec91dd8c96b
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-13 15:59:03 -0500

    write Max Stock Profit algorithm with O(n) linear-runtime

---
+++ MaxStockProfit.js
@@ -0,0 +1,9 @@
+/* Write a function that takes in array of prices as a parameter and
+returns the max possible profit of the day */
+
+maxStockProfit = pricesArr => {
+
+}
+
+//testing
+maxStockProfit([32,46,26,38,40,48,42]); //result = 22
