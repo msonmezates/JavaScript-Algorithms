bb532f8f487f28679cc61d0edb8a3ad7c23155e0
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-09 19:47:09 -0600

    add alternative solution for palindrome

--- Palindrome.js
+++ Palindrome.js
@@ -32,3 +32,16 @@ isPalindrome = str => {
 }
 
 isPalindrome("Madam I'm Adam");
+
+//Another way to solve for a word
+isPalindrome = str => {
+  var i, len = str.length;
+  for(i =0; i<len/2; i++){
+    if (str[i]!== str[len -1 -i])
+       return false;
+  }
+  return true;
+}
+
+isPalindrome('madam'); //true
+isPalindrome("Madam I'm Adam"); //false bc of '
